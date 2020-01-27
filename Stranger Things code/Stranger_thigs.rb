c972d3d6ce4b245acc7dc443c3159eacf33e478c
Idiaz7 <45011735+ismaeldiaz7@users.noreply.github.com>
2020-01-27 13:08:09 -0800

    Add files via upload

---
+++ Stranger Things code/Stranger_thigs.rb
@@ -0,0 +1,18 @@
+live_loop:Loop do
+  use_synth :hoover
+  play :c4, rate: 0.5, amp: 20
+  sleep 0.2
+  play :e4, rate: 0.5, amp: 20
+  sleep 0.2
+  play :g4, rate: 0.5, amp: 20
+  sleep 0.2
+  play :b4, rate: 0.5, amp: 20
+  sleep 0.2
+  play :c5, rate: 0.5, amp: 20
+  sleep 0.2
+  play :b4, rate: 0.5, amp: 20
+  sleep 0.2
+  play :g4, rate: 0.5, amp: 20
+  sleep 0.2
+  play :c4, rate: 0.5, amp: 20
+end
